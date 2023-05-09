5dd7a66a2a6e4afdc6219dc9136d9b3b9475fea7
bee-p <weileenw@naver.com>
2023-05-09 23:38:30 +0900

    [Add] 화살 피하기 게임 구현

---
+++ Assets/ArrowGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    float span = 1.0f;
+    float delta = 0;
+
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+
+        if (this.delta > this.span) // 1초(span 값 기준)마다 생성하도록
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(arrowPrefab) as GameObject;
+            
+            int px = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, 7, 0);
+
+            go.GetComponent<ArrowController>().speed = Random.Range(0.01f, 0.5f);    // 속도 랜덤 부여
+        }
+    }
+}
